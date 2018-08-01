81471a31ff15cecfbe9982e65d065d4e78fefec5
Lakindu Gunasekara <lakindugunasekara@Lakindus-MacBook-Air.local>
2018-08-01 10:02:28 +0530

    remove cosmosdb old service

--- src/services/azure-cosmosdb/cosmosdb-image/package_scripts/getaddr.ps1
+++
@@ -1,4 +0,0 @@
-$addrObj = (Get-NetIPAddress -AddressFamily IPV4 -AddressState Preferred -PrefixOrigin Manual | Select IPAddress)
-$addr = $addrObj.IPAddress
-$result = "Emulator Endpoint: https://" + $addr + ":8081/"
-echo $result
--- src/services/azure-cosmosdb/cosmosdb-image/run.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-md %LOCALAPPDATA%\CosmosDBEmulatorCert 2>nul
-docker run -v %LOCALAPPDATA%\CosmosDBEmulatorCert:c:\CosmosDBEmulator\CosmosDBEmulatorCert -P -t -i cosmosdb_emulator
