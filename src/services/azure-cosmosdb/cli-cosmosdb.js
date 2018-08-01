e72cd6c8161e93ffa4180c400ec2655d292af1d0
Lakindu Gunasekara <lakindugunasekara@Lakindus-MacBook-Air.local>
2018-08-01 10:04:25 +0530

    adding cosmosdb new service

---
+++ src/services/azure-cosmosdb/cli-cosmosdb.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const chalk = require("chalk");
+const AzureCosmosDB = require("./azure-cosmosdb");
+const cosmosdb = new AzureCosmosDB();
+
+const action = () => {
+  try {
+    console.log(chalk.blueBright("Starting Azure Cosmos DB ..."));
+    const res = cosmosdb.start();
+  } catch (err) {
+    console.log(chalk.blueBright.bgRed(err));
+  }
+};
+
+module.exports = {
+  commandName: "cosmosdb-init",
+  action: action
+};
