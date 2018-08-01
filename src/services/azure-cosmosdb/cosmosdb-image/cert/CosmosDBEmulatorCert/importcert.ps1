1af8fee2995b631b1cc8d65d11c06ede048c37ff
lakindu95 <lakindu1995@gmail.com>
2018-08-01 10:02:28 +0530

    remove cosmosdb old service

--- src/services/azure-cosmosdb/cosmosdb-image/cert/CosmosDBEmulatorCert/importcert.ps1
+++
@@ -1,3 +0,0 @@
-$password = ConvertTo-SecureString -String "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==" -Force -AsPlainText
-Import-PfxCertificate -FilePath $PSScriptRoot\CosmosDBEmulatorCert.pfx cert:\localMachine\my -Password $password | Out-Null
-Import-PfxCertificate -FilePath $PSScriptRoot\CosmosDBEmulatorCert.pfx cert:\localMachine\root -Password $password | Out-Null
