9def1ceca78aa766bdf81d3015f81be52ddb21b9
lakindu95 <lakindugunasekara@Lakindus-MacBook-Air.local>
2018-08-03 14:46:22 +0530

    removing obselete code

--- src/services/azure-storage/tests/azure-storage.test.js
+++ src/services/azure-storage/tests/azure-storage.test.js
@@ -25,15 +25,3 @@ test("Blob List returns true with an object", t => {
   const res = http.get(blobUrl + "/devstoreaccount1?comp=list");
   t.true(typeof res === "object");
 });
-
-// test('Blob response status', async t => {
-//   const res = await http.getResponse(blobUrl+"/devstoreaccount1?comp=list")
-//   .catch(err => t.is(err.statusCode, 501));;
-//   t.is(res.statusCode, 200);
-// });
-
-// test('Table response status', async t => {
-//   const res = await http.getResponse(tableUrl+"/devstoreaccount1/mytable")
-//   .catch(err => t.is(err.statusCode, 501));
-//   t.is(res.statusCode, 200);
-// });
