208af0dcdc2f69cd69b3fafe131d607ef93fcaff
lakindu95 <lakindugunasekara@Lakindus-MacBook-Air.local>
2018-08-03 14:46:22 +0530

    removing obselete code

---
+++ src/.temp/testStorage.js
@@ -0,0 +1,11 @@
+// test('Blob response status', async t => {
+//   const res = await http.getResponse(blobUrl+"/devstoreaccount1?comp=list")
+//   .catch(err => t.is(err.statusCode, 501));;
+//   t.is(res.statusCode, 200);
+// });
+
+// test('Table response status', async t => {
+//   const res = await http.getResponse(tableUrl+"/devstoreaccount1/mytable")
+//   .catch(err => t.is(err.statusCode, 501));
+//   t.is(res.statusCode, 200);
+// });
