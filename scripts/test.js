591b0877367015d9bc8926fc11e9b8825daba4e9
Noah Klayman <noahklayman@gmail.com>
2018-10-26 12:16:56 -0700

    fix(tests): allow args to be passed through test script, fix coverage

--- scripts/test.js
+++ scripts/test.js
@@ -2,15 +2,16 @@ const execa = require("execa");
 
 const arg = process.argv[2];
 
-const testPath = arg
-  ? // If an arg is passed, test that module
-    `src/services/azure-${arg}/tests/*.test.js`
-  : // Otherwise, test all modules
-    "src/services/**/tests/*.test.js";
+const testPath =
+  arg && !/^-/.test(arg)
+    ? // If an arg is passed, test that module
+      `src/services/azure-${arg}/tests/*.test.js`
+    : // Otherwise, test all modules
+      "src/services/**/tests/*.test.js";
 
-console.log(`Testing ${arg || "all"}`);
+console.log(`Testing ${arg && !/^-/.test(arg) ? arg : "all"}`);
 
 // Run tests
-return execa("ava", [testPath, "--verbose"], {
+return execa("ava", [testPath, ""], {
   stdio: "inherit"
 });
