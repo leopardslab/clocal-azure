869f0bf54bbd2df27d4e2c5cbae287e599827f9c
Jakub Mularski <jakubmularski723@gmail.com>
2018-10-26 16:28:51 +0200

    fix(compose): Changed CosmosDB key to correct one, and source to . to run on Ubuntu

--- compose.sh
+++ compose.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source ./compose.env
+. ./compose.env
 
 env | grep FUNCTIONFOLDER
-env | grep COSMODBSFOLDER
+env | grep COSMOSFOLDER
 env | grep APIFOLDER
 FUNCTIONFOLDER=env | grep FUNCTIONFOLDER
-COSMODBSFOLDER=env | grep COSMODBSFOLDER
+COSMOSFOLDER=env | grep COSMODBSFOLDER
 APIFOLDER=env | grep APIFOLDER
 sudo -E docker-compose config
 docker-compose up
