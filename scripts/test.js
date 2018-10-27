1149e16b75fdb8b2f7f03f83feb3e99139c24882
Noah Klayman <noahklayman@gmail.com>
2018-10-27 15:19:24 -0700

    fix(tests): allow test script to pass args to ava

--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,31 @@
 const execa = require("execa");
 
-const arg = process.argv[2];
+const args = process.argv;
+args.splice(0, 2);
+const moduleToTest = process.argv[0];
 
-const testPath =
-  arg && !/^-/.test(arg)
-    ? // If an arg is passed, test that module
-      `src/services/azure-${arg}/tests/*.test.js`
-    : // Otherwise, test all modules
-      "src/services/**/tests/*.test.js";
+let testPath;
 
-console.log(`Testing ${arg && !/^-/.test(arg) ? arg : "all"}`);
+if (moduleToTest && !/^-/.test(moduleToTest)) {
+  // Remove module path
+  args.shift();
+  // Test specified module
+  testPath = `src/services/azure-${moduleToTest}/tests/*.test.js`;
+} else {
+  // Test all modules
+  testPath = "src/services/**/tests/*.test.js";
+}
+
+console.log(
+  `Testing ${moduleToTest && !/^-/.test(moduleToTest) ? moduleToTest : "all"} ${
+    args.length > 0 ? `with args: ${args.join(" ")}` : ""
+  }`
+);
+
+// Start service
+execa("sh", ["./sh"]);
 
 // Run tests
-return execa("ava", [testPath, ""], {
+return execa("ava", [testPath, ...args], {
   stdio: "inherit"
 });
