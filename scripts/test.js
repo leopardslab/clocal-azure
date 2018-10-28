b1e6c6e530d34973391e3847be8c7af34214cbcf
Noah Klayman <noahklayman@gmail.com>
2018-10-28 11:12:17 -0700

    feat(tests): automatically start and stop services

--- scripts/test.js
+++ scripts/test.js
@@ -22,10 +22,44 @@ console.log(
   }`
 );
 
-// Start service
-execa("sh", ["./sh"]);
+// Start services
+const api = execa(
+  "./src/bin/clocal-azure.js",
+  ["api-start", "./", "api.json"],
+  {
+    stdio: "inherit"
+  }
+);
+const storage = execa("./src/bin/clocal-azure.js", ["storage-start"], {
+  stdio: [null, "inherit", "inherit"]
+});
+const functions = execa(
+  "./src/bin/clocal-azure.js",
+  ["function-init", "function-sample"],
+  {
+    stdio: [null, "inherit", "inherit"]
+  }
+);
+setTimeout(() => {
+  functions.stdin.write("clocal function-start");
+}, 2000);
+// const functions = execa('./src/')
 
 // Run tests
-return execa("ava", [testPath, ...args], {
-  stdio: "inherit"
-});
+setTimeout(async () => {
+  const ava = execa("ava", [testPath, ...args], {
+    stdio: "inherit"
+  });
+  ava.on("exit", code => {
+    // Stop services
+    api.kill("SIGTERM");
+    storage.stdin.write("clocal storage-stop\n");
+    functions.stdin.write("clocal function-stop\n");
+  });
+  ava.on("error", code => {
+    // Stop services
+    api.kill("SIGTERM");
+    storage.stdin.write("clocal storage-stop\n");
+    functions.stdin.write("clocal function-stop\n");
+  });
+}, 5000);
