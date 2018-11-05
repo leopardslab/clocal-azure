2bfa6bd25f6ebffa617c3424cde903432a4144e2
Jakub Mularski <jakubmularski723@gmail.com>
2018-11-05 21:12:37 +0100

    Added unit test for azure storage

--- src/services/azure-storage/tests/azure-storage.test.js
+++ src/services/azure-storage/tests/azure-storage.test.js
@@ -6,6 +6,23 @@ const queueUrl = "http://localhost:9570";
 const tableUrl = "http://localhost:9571";
 const urlPath = "/devstoreaccount1";
 
+const Docker = require("dockerode");
+const tar = require("tar-fs");
+
+let docker = new Docker({
+  socketPath: "/var/run/docker.sock"
+});
+
+function timeout(ms, fn) {
+  return function(t) {
+    setTimeout(() => {
+      t.fail("Timeout error!");
+      t.end();
+    }, ms);
+    fn(t);
+  };
+}
+
 test("Blob port check", t => {
   const res = http.get(blobUrl + urlPath);
   t.is(res.port, "9569");
@@ -25,3 +42,23 @@ test("Blob List returns true with an object", t => {
   const res = http.get(blobUrl + "/devstoreaccount1?comp=list");
   t.true(typeof res === "object");
 });
+
+test(
+  "Build image of storage",
+  timeout(10000, t => {
+    timeout(t, 10000);
+    function handler(err, stream) {
+      t.is(err, null);
+      t.is(stream, true);
+      stream.pipe(
+        process.stdout,
+        {
+          end: true
+        }
+      );
+    }
+    let tarStream = tar.pack("./example/azure-functions/function-sample");
+    docker.buildImage(tarStream, {}, handler);
+    t.pass();
+  })
+);
