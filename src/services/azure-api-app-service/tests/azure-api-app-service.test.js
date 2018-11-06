d7f5f9bc6852148755f35e5f752bd9160f1fc363
anuragparida <anuragparida37@gmail.com>
2018-11-07 05:06:17 +0530

    Add unit test to Azure API App Service

--- src/services/azure-api-app-service/tests/azure-api-app-service.test.js
+++ src/services/azure-api-app-service/tests/azure-api-app-service.test.js
@@ -1,6 +1,22 @@
 import test, { beforeEach, afterEach } from "ava";
 import http from "ava-http";
 const APIUrl = "http://localhost:9567";
+const Docker = require("dockerode");
+const tar = require("tar-fs");
+
+let docker = new Docker({
+  socketPath: "/var/run/docker.sock"
+});
+
+function timeout(ms, fn) {
+  return function (t) {
+    setTimeout(() => {
+      t.fail("Timeout error!");
+      t.end();
+    }, ms);
+    fn(t);
+  };
+}
 
 test("API app port check", t => {
   const res = http.get(APIUrl);
@@ -16,3 +32,31 @@ test("API app response status", async t => {
   const res = await http.getResponse(APIUrl);
   t.is(res.statusCode, 200);
 });
+
+test(
+  "Build Image for API App Service",
+  timeout(45000, t => {
+    timeout(t, 45000);
+    function handler(err, stream) {
+      t.is(err, null);
+      t.is(stream, true);
+      stream.pipe(
+        process.stdout,
+        {
+          end: true
+        }
+      );
+      stream.on("end", function () {
+        done();
+      });
+    }
+
+    let tarStream = tar.pack("./src/services/azure-api-app-service/Image");
+    docker.buildImage(
+      tarStream,
+      {
+        t: "azure-api-image"
+      }, handler);
+    t.pass();
+  })
+);
