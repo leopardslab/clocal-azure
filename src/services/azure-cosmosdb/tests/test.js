4195d9adf9d61c486d2610ae5be1e8b0ed142136
GeethOnion <41124555+GeethOnion@users.noreply.github.com>
2018-11-10 20:13:53 +0530

    Updated tests

--- src/services/azure-cosmosdb/tests/test.js
+++ src/services/azure-cosmosdb/tests/test.js
@@ -1,10 +1,15 @@
 import test from 'ava';
+import http from 'ava-http';
 
-test('foo', t => {
-	t.pass();
+test('foo should succeed', t => {
+	const res = await http.get('http://localhost/');
+	t.true(typeof res === 'object'); // json object by default
+	t.deepEqual(res, {expected: 'output'}); // deepEqual comparison
 });
 
-test('bar', async t => {
-	const bar = Promise.resolve('bar');
-	t.is(await bar, 'bar');
+test('bar should error', t => {
+	http.post('http://localhost/').catch(err => {
+		t.is(err.statusCode, 400);
+		t.deepEqual(err.response.body, {error: 'message'});
+	});
 });
