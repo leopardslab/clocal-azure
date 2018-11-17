b97cf63acc99d447bd0cea9e2d982c0bf5761c99
kunalbhatia18 <39365087+kunalbhatia18@users.noreply.github.com>
2018-11-17 19:45:10 +0530

    Create wrapper.js

---
+++ function_test/wrapper.js
@@ -0,0 +1,16 @@
+"use strict";
+ 
+import test from 'ava';
+ 
+function has_required_params(req) {
+    return false;
+}
+ 
+module.exports.has_required_params = has_required_params;
+function has_required_params(req) {
+    return Boolean(req.query.year) 
+        && Boolean(req.query.month)
+        && Boolean(req.query.day);
+}
+ 
+module.exports.has_required_params = has_required_params;
