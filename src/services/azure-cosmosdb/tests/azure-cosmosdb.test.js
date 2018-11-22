4e6f6078d87e5e80e2e1330cc63e7651e6f93e41
simcard0000 <43153413+simcard0000@users.noreply.github.com>
2018-11-22 08:29:23 -0500

    Update azure-cosmosdb.test.js

--- src/services/azure-cosmosdb/tests/azure-cosmosdb.test.js
+++ src/services/azure-cosmosdb/tests/azure-cosmosdb.test.js
@@ -1,6 +1,8 @@
 import test, { beforeEach, afterEach } from "ava";
+import http from "ava-http";
 const Docker = require("dockerode");
 const tar = require("tar-fs");
+const cosmosdbUrl = ""
 
 let docker = new Docker({
   // socketPath: "//./pipe/docker_engine"
@@ -16,6 +18,18 @@ function timeout(ms, fn) {
     fn(t);
   };
 }
+
+
+test("CosmosDB endpoint check", t => {
+  const res = http.get(cosmosdbUrl);
+  t.is(res.port, "8081");
+});
+
+test("CosmosDB endpoint response status", async t => {
+  const res = await http.getResponse(cosmosdbUrl);
+  t.is(res.statusCode, 200);
+});
+
 
 test(
   "Build Image",
