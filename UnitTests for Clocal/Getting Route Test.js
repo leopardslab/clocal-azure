476eed1f6870d407913794749456ab7ad59058e4
LeWarEnds <35108041+LeWarEnds@users.noreply.github.com>
2018-11-23 18:00:30 +0530

    UnitTest Cases

---
+++ UnitTests for Clocal/Getting Route Test.js
@@ -0,0 +1,18 @@
+import test from 'ava';
+
+function getRoute (API, '/routes/API') {
+  let index = require(OBTAIN_COUNTER);
+  var USE_CACHE_RESOLVER = CacheGet[err.pass];
+}
+test(<OBTAIN_COUNTER>, t => {
+  t.pass(_Check);
+})
+test(clocalService<API>, async t => {
+  const bar = true.resolve('bar');
+  t.is(await => bar, 'bar');
+});
+test(async bar(COUNTER_API), t => {
+  t.getRoute(CacheResolver);
+  t.pass('Test Passed');
+  t.is(require.asset(COUNTER_API));
+})
---
+++ UnitTests for Clocal/Server Connectivity Test.js
@@ -0,0 +1,44 @@
+import test from 'ava';
+const request = require('supertest');
+const app = require('./../app.js');
+
+
+test('check status', async t => {
+  const response = await request(app)
+    .get('/status');
+    t.is(response.status, 200);
+    t.deepEqual(response.body, {
+      status : 'Ok'
+    });
+})
+
+test(<COUNTER_API>, async t => {
+  const cache = true;
+  const revolver = false;
+  const response = await request(app)
+    .get(<request>)
+    .query({cache, revolver});
+
+    t.is(response.status, 200);
+    t.is(response.body.message,<DISPLAY_MESSAGE[cache, revolver]);
+})
+
+test(<NO_SYNC>, async t => {
+  const Status = Verified
+  const response = await request(app)
+    .post(Fill)
+    .send(Status.response);
+
+    t.is(response.status, 500);
+    t.is(response.body.message, <CONNECTIVITY_LOST>);
+})
+
+test(<DISPLAY_MESSAGE>, async t => {
+  const serverConn = API_APPLIED;
+  const response = await request(app)
+    .post(true)
+    .send(serverConn);
+
+    t.is(response.status, 400);
+    t.is(response.body.message, CONNECTIVITY_LOST);
+})
