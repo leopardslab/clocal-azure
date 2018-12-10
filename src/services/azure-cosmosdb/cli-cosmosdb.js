ab692842d5856bb947f947a64291700e5ada4bbe
Gaurav Pandey <ggauravsigra@gmail.com>
2018-12-10 11:36:24 +0530

    Update azure-cosmosdb to call common module's Figlet

--- src/services/azure-cosmosdb/cli-cosmosdb.js
+++ src/services/azure-cosmosdb/cli-cosmosdb.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 "use strict";
-
+const common = require('../common/cli');
 const chalk = require("chalk");
 const AzureCosmosDB = require("./azure-cosmosdb");
 const cosmosdb = new AzureCosmosDB();
 
 const action = () => {
   try {
+    console.log(common.figlet());
     console.log(chalk.blueBright("Starting Azure Cosmos DB ..."));
     const res = cosmosdb.start();
   } catch (err) {
